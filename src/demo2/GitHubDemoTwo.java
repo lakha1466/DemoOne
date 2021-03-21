626ef3d3cb8e2f3bbc285745545aaae496ff7cd5
lakha <lakha@DESKTOP-TF1KP1G.myfiosgateway.com>
2021-03-21 15:36:28 -0400

    addes nameIs

--- src/demo2/GitHubDemoTwo.java
+++ src/demo2/GitHubDemoTwo.java
@@ -4,5 +4,11 @@ public class GitHubDemoTwo {
 public static void main(String[] args) {
 	
 	System.out.println("My First Github Commit...");
+	nameIs();
+
+}
+public static void nameIs() {
+	System.out.println("Name Is: Lakhan");
+
 }
 }

e90e658f2654a21273097ac335559e1e927d0c85
lakha <lakha@DESKTOP-TF1KP1G.myfiosgateway.com>
2021-03-21 22:15:52 -0400

    isEqual

--- src/demo2/GitHubDemoTwo.java
+++ src/demo2/GitHubDemoTwo.java
@@ -3,9 +3,12 @@ package demo2;
 public class GitHubDemoTwo {
 public static void main(String[] args) {
 	int a =5;
+	String strOne = "Java";
+	String strTwo = "java";
 	System.out.println("My First Github Commit...");
 	nameIs();
     isOdd(a);
+    isEqual(strOne, strTwo);
 }
 public static void nameIs() {
 	System.out.println("My Name Is Lakhan");
@@ -20,5 +23,17 @@ public static boolean isOdd(int a ) {
 		return false;
 	
 	}
+}
+
+
+public static void isEqual(String strOne, String strTwo) {
+	
+	if(strOne.equals(strTwo)) {
+		System.out.println("They Are Even");
+	}else {
+		System.out.println("They Are Not Even");
+	}
+	
+	
 }
 }

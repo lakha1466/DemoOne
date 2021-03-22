2d57563f719b506e7f416123b89dd374e2e05896
lakha <lakha@DESKTOP-TF1KP1G.myfiosgateway.com>
2021-03-21 22:06:12 -0400

    Initial Commit

--- src/demo2/GitHubDemoTwo.java
+++ src/demo2/GitHubDemoTwo.java
@@ -2,13 +2,23 @@ package demo2;
 
 public class GitHubDemoTwo {
 public static void main(String[] args) {
-	
+	int a =5;
 	System.out.println("My First Github Commit...");
 	nameIs();
-
+    isOdd(a);
 }
 public static void nameIs() {
 	System.out.println("My Name Is Lakhan");
 
+}
+
+public static boolean isOdd(int a ) {
+  
+	if(a % 2 ==0) {
+		return true;
+	}else{
+		return false;
+	
+	}
 }
 }
